ae929d3f047a02d509694512a81a004f2564fc68
Yossi <yossi@motro.net>
2019-03-11 13:33:35 +0200

    Correcting markup for JSX.

--- src/components/youtube_player.js
+++ src/components/youtube_player.js
@@ -2,5 +2,5 @@ import React from 'react';
 
 export const YouTubePlayer = ({videoId}) => (
     <iframe id="ytplayer" type="text/html" width="640" height="360"
-            src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com`} frameborder="0"/>
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com`} frameBorder="0"/>
 );
