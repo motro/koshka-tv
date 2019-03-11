9e1bfa3883ea67306737dca32aa7589c722e458e
Yossi <yossi@motro.net>
2019-03-11 12:46:29 +0200

    Fetch YouTube videos example.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import './App.css';
 import {Header} from "./components/header";
 
 export class App extends Component {
+
+    componentDidMount() {
+        this.fetchVideos('cats');
+    }
+
+    async fetchVideos(criteria) {
+        const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${criteria}&key=${process.env.REACT_APP_ACCESS_KEY}`;
+        const response = await fetch(url);
+        const json = await response.json();
+        const {items} = json;
+
+        console.log(items);
+    }
+
     render() {
         return (
             <div className="App">
