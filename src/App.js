1610bae2588b6e185ed48d659cf0014638e75ebd
Yossi <yossi@motro.net>
2019-03-11 12:48:49 +0200

    Set state object to contain fetched videos.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import {Header} from "./components/header";
 
 export class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+    }
 
     componentDidMount() {
         this.fetchVideos('cats');
@@ -12,9 +16,9 @@ export class App extends Component {
         const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${criteria}&key=${process.env.REACT_APP_ACCESS_KEY}`;
         const response = await fetch(url);
         const json = await response.json();
-        const {items} = json;
+        const videos = json.items;
 
-        console.log(items);
+        this.setState({videos});
     }
 
     render() {
